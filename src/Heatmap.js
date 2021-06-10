d2383ed6aff2798c1b8ce0f653268b181dafa9ba
blackpatton17 <zehuaz4@uw.edu>
2021-06-09 19:18:00 -0700

    123

--- src/Heatmap.js
+++ src/Heatmap.js
@@ -1,26 +1,133 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import rd3 from 'react-d3-library';
-import node from './heatmap_vanilla';
+import * as d3 from "d3";
 const RD3Component = rd3.Component;
 
-class Heatmap extends React.Component {
+const Heatmap = () => {
+  const [d3node, setD3node] = useState();
 
-  constructor(props) {
-    super(props);
-    this.state = {d3: ''}
+  const createSVG = () => {
+    var node = document.createElement('div');
+
+    var margin = {top: 80, right: 25, bottom: 30, left: 40},
+      width = 450 - margin.left - margin.right,
+      height = 450 - margin.top - margin.bottom;
+
+    var svg = d3.select(node)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
+
+      // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+      var myGroups = d3.map(data, function(d){return d.group;}).keys()
+      var myVars = d3.map(data, function(d){return d.variable;}).keys()
+
+      // Build X scales and axis:
+      var x = d3.scaleBand()
+        .range([ 0, width ])
+        .domain(myGroups)
+        .padding(0.05);
+      svg.append("g")
+        .style("font-size", 15)
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickSize(0))
+        .select(".domain").remove()
+
+      // Build Y scales and axis:
+      var y = d3.scaleBand()
+        .range([ height, 0 ])
+        .domain(myVars)
+        .padding(0.05);
+      svg.append("g")
+        .style("font-size", 15)
+        .call(d3.axisLeft(y).tickSize(0))
+        .select(".domain").remove()
+
+      // Build color scale
+      var myColor = d3.scaleSequential()
+        .interpolator(d3.interpolateInferno)
+        .domain([1,100])
+
+      // create a tooltip
+      var tooltip = d3.select("#my_dataviz")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+
+      // Three function that change the tooltip when user hover / move / leave a cell
+      var mouseover = function(d) {
+        tooltip
+          .style("opacity", 1)
+        d3.select(this)
+          .style("stroke", "black")
+          .style("opacity", 1)
+      }
+      var mousemove = function(d) {
+        tooltip
+          .html("The exact value of<br>this cell is: " + d.value)
+          .style("left", (d3.mouse(this)[0]+70) + "px")
+          .style("top", (d3.mouse(this)[1]) + "px")
+      }
+      var mouseleave = function(d) {
+        tooltip
+          .style("opacity", 0)
+        d3.select(this)
+          .style("stroke", "none")
+          .style("opacity", 0.8)
+      }
+
+      // add the squares
+      svg.selectAll()
+        .data(data, function(d) {return d.group+':'+d.variable;})
+        .enter()
+        .append("rect")
+        .attr("x", function(d) { return x(d.group) })
+        .attr("y", function(d) { return y(d.variable) })
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", x.bandwidth() )
+        .attr("height", y.bandwidth() )
+        .style("fill", function(d) { return myColor(d.value)} )
+        .style("stroke-width", 4)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+
+      console.log("bar");
+    })
+      .then(() => {
+        svg.append("text")
+          .attr("x", 0)
+          .attr("y", -50)
+          .attr("text-anchor", "left")
+          .style("font-size", "22px")
+          .text("A d3.js heatmap");
+        console.log("foo")
+        setD3node(node)
+      })
   }
 
-  componentDidMount() {
-    this.setState({d3: node});
-  }
+  useEffect(() => {
+    createSVG()
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <RD3Component data={this.state.d3} />
-      </div>
-    )
-  }
-};
+  return (
+    <div>
+      <RD3Component data={d3node} />
+    </div>
+  )
+}
 
-export default Heatmap;
+export default Heatmap
